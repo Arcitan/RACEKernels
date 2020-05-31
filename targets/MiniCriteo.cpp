65dd8c0343265cd93425ade536cdb0dfecc88d49
Franklin <franklinzgma@gmail.com>
2020-05-31 18:31:58 -0500

    fixed some leftover debugging stuff

--- targets/MiniCriteo.cpp
+++ targets/MiniCriteo.cpp
@@ -33,9 +33,7 @@ int main() {
 	// if running with cmake, use parent directory; otherwise check working directory and change relative path
 	std::ifstream data_file("../dat/criteo_mini.txt");
 	std::vector<std::vector<double>> data;
-//	ReadData(data_file, data);
-	std::vector<double> d;
-	VectorFeatures(data_file, d);
+	ReadData(data_file, data);
 
 	/* Verify everything got read properly */
 //	for (const auto& r : data) {
@@ -55,12 +53,10 @@ int main() {
 	MinHash mh = MinHash(26, L);
 	int* hashes = new int[L];
 	int* minHashes = new int[L];
-	std::cout << data.size() << std::endl;
 
 	for (const auto& row : data) {
 		std::vector<double> intFeatures = std::vector<double>(row.begin(), row.begin() + 13);
 		std::vector<double> catFeatures = std::vector<double>(row.begin() + 13, row.end());
-
 
 		l2.getHash(intFeatures, hashes);
 		mh.getHash(catFeatures, minHashes);
@@ -73,5 +69,7 @@ int main() {
 		sketch.add(hashes);
 	}
 	delete[] hashes;
+
+	sketch.pprint(std::cout);
 
 }
